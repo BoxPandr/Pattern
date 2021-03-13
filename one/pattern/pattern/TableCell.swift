3fbc60ea9b390195c3fe91b8fdc6da17858053fa
邓江洲 <dengjiangzhouReal@gmail.com>
2021-03-13 19:08:44 +0800

    pattern one

---
+++ one/pattern/pattern/TableCell.swift
@@ -0,0 +1,40 @@
+//
+//  TableCell.swift
+//  pattern
+//
+//  Created by Jz D on 2021/3/13.
+//
+
+import UIKit
+
+class TableCell: UITableViewCell {
+    
+    
+    
+    var beSelected: Bool = false{
+        didSet{
+            if beSelected{
+                backgroundColor = UIColor.red
+            }
+            else{
+                backgroundColor = UIColor.white
+            }
+        }
+    }
+    
+    
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ one/pattern/pattern/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  pattern
+//
+//  Created by Jz D on 2021/3/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    
+    
+    @IBOutlet weak var lhs: UIButton!
+    
+    
+    
+    @IBOutlet weak var rhs: UIButton!
+    
+    
+    
+    
+    @IBOutlet weak var table: UITableView!
+    
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+
+
+}
+
